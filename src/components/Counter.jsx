b538f06209eb157057326cb7cb51c18ecba6cbc1
valpas312 <96994527+valpas312@users.noreply.github.com>
2023-04-03 23:59:29 -0300

    agregando rtk

---
+++ src/components/Counter.jsx
@@ -0,0 +1,28 @@
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, reset } from "./reducers/counterSlice"; 
+
+export function Counter() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+      <button onClick={() => dispatch(reset())}>Reset Counter</button>
+    </div>
+  );
+}
